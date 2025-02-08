145b24938f0b848f3ebcac5a4135b6582a3d8fa2
Zack Guard <5311230+z-------------@users.noreply.github.com>
2025-02-08 17:58:27 +0800

    Don't hint when the caught exception is the closest common superclass

--- tests/exception/tbroad.nim
+++ tests/exception/tbroad.nim
@@ -9,3 +9,28 @@ try:
 except CatchableError: #[tt.Hint
        ^ Only ValueError is raised here which is more specific [ExceptTooBroad] ]#
   discard
+
+type
+  CommonError = object of CatchableError
+  FooError = object of CommonError
+  BarError = object of CommonError
+
+proc multi() =
+  if true:
+    raise (ref FooError)()
+  else:
+    raise (ref BarError)()
+
+try:
+  multi()
+except CatchableError: #[tt.Hint
+       ^ Only CommonError is raised here which is more specific [ExceptTooBroad] ]#
+  discard
+
+try:
+  multi()
+except BarError:
+  discard
+except CommonError: #[tt.Hint
+       ^ Only FooError is raised here which is more specific [ExceptTooBroad] ]#
+  discard
